f1afaa9f82d65f6017e5dc3446ac8c8125ae4fa0
beingbravado <sharmagyanesh199@gmail.com>
2021-06-09 10:29:20 +0530

    min and max

--- B1065.java
+++ B1065.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class B1065 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int m= scan.nextInt();
-        int min= m+1>=n?0:n-m-1;
-        int k= (int)Math.sqrt(2*n);
-        scan.close();;
+        long n = scan.nextLong();
+        long m = scan.nextLong();
+        long min = m * 2 >= n ? 0 : n - 2 * m;
+        long max = 0;
+        int x=0;
+        for (int i = 1; i <= 2*m; i++) {
+            max += i - 1;
+            if (max >= m) {
+                x=i;
+                break;
+            }
+        }
+        max = n - x;
+        System.out.println(min + " " + max);
+        scan.close();
+        ;
     }
 }
